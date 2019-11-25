cc2848f9be3c328a10eccddcfe2756d659a8806e
0yzh <55042234+0yzh@users.noreply.github.com>
2019-11-25 11:03:52 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const _regex = /(Jan(uary)?|Feb(ruary)?|Mar(ch)?|Apr(il)?|May|Jun(e)?|Jul(y)?|Aug(ust)?|Sep(tember)?|Oct(ober)?|Nov(ember)?|Dec(ember)?)|(\d{1,2},)|(\d{4}\s)|((0[0-9]|1[0-9]|2[0-3]|[0-9]):[0-5][0-9](am|pm)|(0[0-9]|1[0-9]|2[0-3]|[0-9])(am|pm))|(store.google.com)/g;
 
-// const _regex = /(Jan(uary)?|Feb(ruary)?|Mar(ch)?|Apr(il)?|May|Jun(e)?|Jul(y)?|Aug(ust)?|Sep(tember)?|Oct(ober)?|Nov(ember)?|Dec(ember)?)|(\d{1,2},)|(\d{4})|(0[0-9]|1[0-9]|2[0-3]|[0-9]):[0-5][0-9](am|pm)/g;
 const original = document.querySelector('.original');
 const submit = document.querySelector('.submit');
 const clipboard = document.querySelector('.clipboard');
